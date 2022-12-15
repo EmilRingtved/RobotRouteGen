eab78578131a71a883c3f93c4f784824fa719b6a
EmilRingtved <e.ringtved@gmail.com>
2022-12-15 13:06:50 +0100

    added stop point generation

--- generateRobotPath.py
+++ generateRobotPath.py
@@ -3,6 +3,7 @@
 
 from svg.path import parse_path
 from xml.dom import minidom
+from bs4 import BeautifulSoup
 from matplotlib import pyplot as plt
 import numpy as np
 
@@ -32,12 +33,21 @@ def points_from_path(path, density, scale, offset):
 # with a given density and scale
 def points_from_doc(doc, density=5, scale=1, offset=0):
     offset = offset[0] + offset[1] * 1j
-    points = []
+    route_points = []
+    stop_points = []
+    # Searches the svg file for paths to generate waypoints from
     for element in doc.getElementsByTagName("path"):
         for path in parse_path(element.getAttribute("d")):
-            points.extend(points_from_path(path, density, scale, offset))
+            route_points.extend(points_from_path(path, density, scale, offset))
+    
+    #searches for stop points in the svg file and appends them to the list of stop points
+    for circle in doc.getElementsByTagName("circle"):
+        x = circle.getAttribute('cx')
+        y = circle.getAttribute('cy')
+        stop_point = np.array([x,y])
+        stop_points = np.append(stop_points,stop_point)
 
-    return points
+    return route_points, stop_points
 
 # Convert an SVG path to a sequence of coordinates
 # and return them as numpy arrays
@@ -47,21 +57,11 @@ def svg_to_cords(svg_path):
     x_coords = np.empty(0,float)
 
     doc = minidom.parseString(svg_path)
-    points = points_from_doc(doc, density=1, scale=1, offset=(0, 0))
+    # change density or scale to scale up system
+    points = points_from_doc(doc, density=1, scale=1, offset=(0,0))
     for i in range(len(points)):
         print([points[i][0],points[i][1]])
         x_coords = np.append(x_coords, (points[i][0]))
         y_coords = np.append(y_coords, (points[i][1]))
 
     return x_coords, y_coords
-
-#def coords_to_angles(x,y):
-
-
-svg_file_path = input("Write path to route svg.\n")
-with open(svg_file_path, "r") as f:
-    # Read the contents of the file into a string variable
-    svg_str = f.read()
-x_coords, y_coords = svg_to_cords(svg_str)
-plt.scatter(x_coords,y_coords)
-plt.show()
